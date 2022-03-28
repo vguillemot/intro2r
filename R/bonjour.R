cdeb5149d632a513ecc7518e519fe140a7fabe34
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2022-03-28 15:32:03 +0200

    ADD extdata, fix Venn Diagrams, and fix tutorials

--- R/bonjour.R
+++ R/bonjour.R
@@ -50,3 +50,32 @@ bonjour <- function(couleur = "pink", fond = "limegreen") {
 #' @references \url{https://ciqual.anses.fr/}
 #' @keywords data
 NULL
+
+#' Le jeu de données Nutriwi
+#'
+#' Composition nutritionnelle de 27 plats alimentaires.
+#'
+#' La table fruits est de classe "data.frame" et contient les colonnes suivantes :
+#'\itemize{
+#'  \item ID, identifiant du plat,
+#'  \item Type, type de plat (entrée, plat ou dessert),
+#'  \item Nutriscore, le nutriscore du plats (A, B ou C)
+#'  \item Lien, lien vers le plat sur le site Nutriwi,
+#'  \item Glucides, en grammes pour 100g,
+#'  \item Lipides, en grammes pour 100g,
+#'  \item Proteines, en grammes pour 100g,
+#'  \item Sucre, en grammes pour 100g,
+#'  \item Fibres, en grammes pour 100g,
+#'  \item Sel, en grammes pour 100g,
+#'  \item AGS, Acides Gras Saturés, en grammes pour 100g,
+#'  \item Energie, en kCal pour 100g.
+#'}
+#'
+#' @name nutrimenu
+#' @docType data
+#' @references \url{https://www.nutriwi.com/}
+#' @keywords data
+NULL
+
+
+
