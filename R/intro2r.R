993ca847dc667b949c9181b4c9ce37fabbb06e7f
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2022-03-28 09:21:00 +0200

    NEW version

---
+++ R/intro2r.R
@@ -0,0 +1,4 @@
+#' Package pour la formation "Introduction au Logiciel R"
+#' Contient les vignettes (les slides de cours), le jeu de données "fruits", le jeu de données "nutrimenu"  et les tutoriels.
+#' @keywords internal
+"_PACKAGE"
